13eae8d7bf8e4b67ecf564669c452d23ce24f912
Robert Zondervan <robert@conduction.nl>
2023-02-03 13:36:25 +0100

    Restructure

---
+++ GeboorteVrijBRPBundle.php
@@ -0,0 +1,9 @@
+<?php
+// src/PetStoreBundle.php
+namespace CommonGateway\GeboorteVrijBRPBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GeboorteVrijBRPBundle extends Bundle
+{
+}
